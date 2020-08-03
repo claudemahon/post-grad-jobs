4a115f9b16f660eecc7a33e11906f7b0117006e1
claude.mahon99 <claude.mahon99@gmail.com>
2020-08-02 20:26:41 -0400

    gatsby.js was added to the project. Bulma was added to handle the front end. Sharp is going to be used for the front end. Next to be added is the backend.

--- gatsby-site/src/pages/index.js
+++ gatsby-site/src/pages/index.js
@@ -1,5 +1,17 @@
 import React from "react"
+import 'bulma/css/bulma.css'
+import "./style.css"
 
-export default function Home() {
-  return <div>Hello world!</div>
+const IndexPage = () => {
+  return (
+    <>
+    <section className="hero is-fullheight">
+    <body className="hero-body">
+      <div className="title">hello world</div>
+    </body>
+    </section>
+    </>
+  )
 }
+
+export default IndexPage
